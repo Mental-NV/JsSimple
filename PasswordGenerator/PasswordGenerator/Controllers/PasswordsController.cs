3829cadb86217a0418fb8c0a314b127cbe42ac21
Mental-NV <ruslan.galiev@outlook.com>
2014-06-30 00:14:56 +0600

    Start ASP.Net MVC using

---
+++ PasswordGenerator/PasswordGenerator/Controllers/PasswordsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PasswordGenerator.Controllers
+{
+    public class PasswordsController : ApiController
+    {
+        public IEnumerable<string> GetAll()
+        {
+            return new List<string>() { "AA1", "AA2", "AA3" };
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/Scripts/CallApi.js
@@ -0,0 +1,10 @@
+﻿var uri = "api/passwords";
+
+$(document).ready(function () {
+    $.getJSON(uri)
+        .done(function (data) {
+            $.each(data, function (key, item) {
+                $("<li>", { text: item }).appendTo($("#passwords"));
+            });
+        });
+});
