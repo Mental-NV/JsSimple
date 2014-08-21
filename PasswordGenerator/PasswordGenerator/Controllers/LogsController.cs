b6b4e65592444eb659fa4476e1148c1c5c660801
Mental-NV <ruslan.galiev@outlook.com>
2014-08-22 00:16:52 +0600

    Log user activity into a database

---
+++ PasswordGenerator/PasswordGenerator/Controllers/LogsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PasswordGenerator;
+
+namespace PasswordGenerator.Controllers
+{
+    public class LogsController : ApiController
+    {
+        private LogContext db = new LogContext();
+
+        // GET: api/Logs
+        public IEnumerable<Log> GetLogSet()
+        {
+            return db.LogSet.OrderByDescending(x => x.When).Take(100).AsEnumerable().Reverse();
+        }
+
+        // GET: api/Logs/5
+        [ResponseType(typeof(Log))]
+        public IHttpActionResult GetLog(int id)
+        {
+            Log log = db.LogSet.Find(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(log);
+        }
+
+        // PUT: api/Logs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLog(int id, Log log)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != log.LogId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(log).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Logs
+        [ResponseType(typeof(Log))]
+        public IHttpActionResult PostLog(Log log)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.LogSet.Add(log);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = log.LogId }, log);
+        }
+
+        // DELETE: api/Logs/5
+        [ResponseType(typeof(Log))]
+        public IHttpActionResult DeleteLog(int id)
+        {
+            Log log = db.LogSet.Find(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            db.LogSet.Remove(log);
+            db.SaveChanges();
+
+            return Ok(log);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LogExists(int id)
+        {
+            return db.LogSet.Count(e => e.LogId == id) > 0;
+        }
+    }
+}
