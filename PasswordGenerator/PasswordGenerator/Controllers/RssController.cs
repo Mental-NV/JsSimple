58928125e89b77d3a9186135b5f1d69dab3c9b0c
Mental-NV <ruslan.galiev@outlook.com>
2014-08-23 18:15:40 +0600

    WF activites were added

---
+++ PasswordGenerator/PasswordGenerator/Controllers/RssController.cs
@@ -0,0 +1,40 @@
+﻿using PasswordGenerator.WorkflowActivites;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using System.Web.Http;
+
+namespace PasswordGenerator.Controllers
+{
+    public class RssController : ApiController
+    {
+        // GET: api/Rss
+        public IEnumerable<string> Get()
+        {
+            List<Uri> feedUrls = new List<Uri>()
+            {
+                new Uri("http://www.microsoft.com/ru-ru/news/rss/rssfeed.aspx"),
+                new Uri("http://blogs.msdn.com/b/rudevnews/rss.aspx")
+            };
+
+            var feedResult = new List<SyndicationFeed>();
+
+            WorkflowInvoker.Invoke(new RssFeed(), new Dictionary<string, object>() 
+            { 
+                {"FeedUrls", feedUrls},
+                {"FeedResult", feedResult}
+            });
+
+            var result = new List<string>();
+            foreach (var item in feedResult)
+            {
+                result.AddRange(item.Items.Select(x => x.Title.Text));
+            }
+            return result;
+        }
+    }
+}
